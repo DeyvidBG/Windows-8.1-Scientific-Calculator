39c4f40cc264a653076742b26790734698f69f0a
Deyvid Popov <47706857+DeyvidBG@users.noreply.github.com>
2023-04-29 19:22:54 +0300

    Broken update

--- calculator_gui.py
+++ calculator_gui.py
@@ -1,11 +1,72 @@
 from tkinter import *
 from math import sin, cos, tan, sinh, cosh, tanh, log, log10, pi, radians, degrees, factorial
+import sys
+from decimal import Decimal, DecimalException
+
+max_float = sys.float_info.max
+min_float = -sys.float_info.max
+
+def within_range(value):
+    return (value > min_float) and (value < max_float)
+
+def check_overflow(func):
+    def wrapper(*args):
+        result = func(*args)
+        if not within_range(result):
+            raise OverflowError("Result is too large or too small to be represented as a float.")
+        return result
+    return wrapper
+
+@check_overflow
+def add(temp, a, precision):
+    return round(Decimal(temp + a), precision)
+
+@check_overflow
+def subtract(temp, a, precision):
+    return round(Decimal(temp - a),precision)
+
+@check_overflow
+def multiply(temp, a, precision):
+    return round(Decimal(temp * a),precision)
+
+@check_overflow
+def divide(temp, a, precision):
+    if a == 0:
+        '''
+        if temp == 0:
+            return float('inf')
+        '''
+        raise ValueError("Division by 0 is not allowed.")
+    return round(Decimal(temp / a),precision)
+
+@check_overflow
+def modulo(temp, a, precision):
+    if a == 0:
+        raise ValueError("We cannot get the rest from division by zero.")
+    return round(Decimal(math.fmod(temp,a)), precision)
+
+@check_overflow
+def sqrt(a, precision):
+    if a < 0:
+        raise ValueError("Square root of a negative number is not supported.")
+    return round(Decimal(math.sqrt(a)), precision)
+
+@check_overflow
+def reciproc(a, precision):
+    if a == 0:
+        raise ValueError("Division by 0 is not allowed.")
+    return round(Decimal(1 / a), precision)
+
+@check_overflow
+def change_sign(a, precision):
+    return round(Decimal(-a), precision)
 
 memory = 0
 temp_result = 0
-equation = "0"
+equation = ""
 counter = 0
 history = {}
+digit = True
 
 # work with memory
 def addToHistory(counter, history, equation, result):
@@ -25,6 +86,7 @@ def clearLastEntry(counter, history):
         counter -= 1
         return counter, history, history[counter-1]["equation"], history[counter-1]["result"]
 
+
 # Create window
 window = Tk()
 window.title("Scientific Calculator")
@@ -38,8 +100,8 @@ for i in range(0, 8):
 for i in range(0,10):
     window.columnconfigure(i, minsize = 50)
 
-label_text = StringVar(value = "0")
-entry_text = StringVar(value = "0")
+label_text = StringVar(value = "")
+entry_text = StringVar(value = "")
 
 # Define label where the equation will be shown
 label = Label(window, textvariable=label_text)
@@ -63,6 +125,36 @@ def create_button(text, row, column, rowspan=1, columnspan=1, on_click=None, wid
     if on_click:
         btn.config(command=on_click)
     return btn
+
+# define functionalities
+
+# add number
+def add_number(number):
+    global digit
+    if digit == True:
+        entry_text.set(entry_text.get() + str(number))
+    else:
+        entry_text.set(str(number))
+        digit = True
+
+# add operator
+def add_operator(operator):
+    global temp_result, equation, counter, digit
+    if digit == True:
+        digit = False
+        counter+=1
+        if operator in ["+","-","*","/"]:
+            equation += entry_text.get()
+            if counter == 1:
+                temp_result += int(entry_text.get())
+                equation += operator
+                entry_text.set("")
+                label_text.set(equation)
+            elif operator == "=":
+                entry_text.set(str(temp_result))
+                label_text.set(equation + "=" + str(temp_result))
+
+
 
 # Left-side buttons
 buttons_left = [
@@ -105,24 +197,24 @@ buttons_right = [
     ("C", 3, 7, 1, 1, None),
     ("±", 3, 8, 1, 1, None),
     ("√", 3, 9, 1, 1, None),
-    ("7", 4, 5, 1, 1, None),
-    ("8", 4, 6, 1, 1, None),
-    ("9", 4, 7, 1, 1, None),
-    ("/", 4, 8, 1, 1, None),
-    ("%", 4, 9, 1, 1, None),
-    ("4", 5, 5, 1, 1, None),
-    ("5", 5, 6, 1, 1, None),
-    ("6", 5, 7, 1, 1, None),
-    ("*", 5, 8, 1, 1, None),
+    ("7", 4, 5, 1, 1, lambda: add_number(7)),
+    ("8", 4, 6, 1, 1, lambda: add_number(8)),
+    ("9", 4, 7, 1, 1, lambda: add_number(9)),
+    ("/", 4, 8, 1, 1, lambda: add_operator("/")),
+    ("%", 4, 9, 1, 1, lambda: add_operator("%")),
+    ("4", 5, 5, 1, 1, lambda: add_number(4)),
+    ("5", 5, 6, 1, 1, lambda: add_number(5)),
+    ("6", 5, 7, 1, 1, lambda: add_number(6)),
+    ("*", 5, 8, 1, 1, lambda: add_operator("*")),
     ("1/x", 5, 9, 1, 1, None),
-    ("1", 6, 5, 1, 1, None),
-    ("2", 6, 6, 1, 1, None),
-    ("3", 6, 7, 1, 1, None),
-    ("-", 6, 8, 1, 1, None),
+    ("1", 6, 5, 1, 1, lambda: add_number(1)),
+    ("2", 6, 6, 1, 1, lambda: add_number(2)),
+    ("3", 6, 7, 1, 1, lambda: add_number(3)),
+    ("-", 6, 8, 1, 1, lambda: add_operator("-")),
     ("=", 6, 9, 2, 1, None),
-    ("0", 7, 5, 1, 2, None),
+    ("0", 7, 5, 1, 2, lambda: add_number(0)),
     (".", 7, 7, 1, 1, None),
-    ("+", 7, 8, 1, 1, None),
+    ("+", 7, 8, 1, 1, lambda: add_operator("+")),
 ]
 
 # Display buttons
